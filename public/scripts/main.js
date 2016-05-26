9ad50a0e3faf76164e380afdfc8c615e630baa2f
Knaarfje <thomasjbosch@gmail.com>
2016-05-26 16:14:45 +0200

    charts to db

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -14,6 +14,7 @@ app.controller("afterburnerCtrl", function ($scope, $firebaseAuth, $firebaseObje
 
     $scope.init = () => {
         $scope.signin("thomas@boerdam.nl", "Batman01");
+        $scope.initChart();
     }
 
     $scope.signin = (email, password) => {
@@ -21,8 +22,11 @@ app.controller("afterburnerCtrl", function ($scope, $firebaseAuth, $firebaseObje
 
         firebase.auth().signInWithEmailAndPassword(email, password).then(function (data) {
             $scope.authData = data;
-            console.log(data);
-            $scope.sprints = $firebaseArray(ref.child("sprints"));
+            $scope.sprints = $firebaseArray(ref.child("sprints").orderByChild('order'));
+            console.log($scope.sprints);
+            $scope.sprints.$loaded(function () {
+                $scope.updateChart();
+            })
         });
     }
 
@@ -33,10 +37,137 @@ app.controller("afterburnerCtrl", function ($scope, $firebaseAuth, $firebaseObje
         });
     }
 
+    $scope.updateChart = () => {
+        var labels = $scope.sprints.map(function (d) {
+            return d.name;
+        });
+        var estimated = $scope.sprints.map(function (d) {
+            return d.velocity;
+        });
+        var burned = $scope.sprints.map(function (d) {
+            var i = 0;
+            for (var x in d.burndown) {
+                i = i + d.burndown[x];
+            }
+            return i;
+        });
+
+        $scope.myBar.data.labels = labels;
+        $scope.myBar.data.datasets[0].data = burned;
+        $scope.myBar.data.datasets[1].data = estimated;
+
+        $scope.myBar.update();
+    }
+
     $scope.addBurndown = (points, sprint) => {
 
         var sprintKey = $scope.sprints.$keyAt(sprint);        
         var burndowns = $scope.sprints.$getRecord(sprintKey).burndown;
         burndowns.$add(points);
     }
+
+    $scope.initChart = () => {
+        $scope.chartCtx = document.getElementById("graph").getContext("2d");
+        $scope.barChartData = {
+            labels: ["Sprint 01", "Sprint 02", "Sprint 03", "Sprint 04", "Sprint 05", "Sprint 06", "Sprint 07"],
+            datasets: [
+              {
+                label: "Gehaald",
+                type:'line',
+                data: [70, 75, 70, 55, 85, 50, 55],
+                fill: false,
+                borderColor: '#EB51D8',
+                backgroundColor: '#EB51D8',
+                pointBorderColor: '#EB51D8',
+                pointBackgroundColor: '#EB51D8',
+                pointHoverBackgroundColor: '#EB51D8',
+                pointHoverBorderColor: '#EB51D8',
+                yAxisID: 'y-axis-2',
+            },{
+                type: 'bar',
+                label: "Geschat",
+                data: [75, 80, 62, 70, 80, 50, 60],
+                fill: false,
+                backgroundColor: '#5FFAFC',
+                borderColor: '#5FFAFC',
+                hoverBackgroundColor: '#5CE5E7',
+                hoverBorderColor: '#5CE5E7',
+                yAxisID: 'y-axis-1',
+            }]
+        };
+
+        $scope.myBar = new Chart($scope.chartCtx, {
+                type: 'bar',
+                data: $scope.barChartData,
+                options: {
+                  responsive: true,
+                  maintainAspectRatio: false,
+                  tooltips: {
+                      mode: 'label',
+                      cornerRadius: 3,
+                  },
+                  elements: {
+                    line: {
+                        fill: false
+                    }
+                  },
+                  legend: {
+                    position: 'bottom',
+                    labels: {
+                      fontColor: '#fff'
+                    },
+                  },
+                  scales: {
+                    xAxes: [{
+                        display: true,
+                        gridLines: {
+                            display: false,
+                            color : "rgba(255,255,255,.1)",
+                        },
+                        ticks: {
+                          fontColor: '#fff'
+                        }
+                    }],
+                    yAxes: [{
+                        type: "linear",
+                        display: true,
+                        position: "left",
+                        id: "y-axis-1",
+                        ticks: {
+                          stepSize: 10,
+                          beginAtZero: true,
+                          fontColor: '#fff',
+                          suggestedMax: 100,
+                        },
+                        gridLines:{
+                            display: true,
+                            color : "rgba(255,255,255,.1)",
+                            drawTicks: false,
+                        },
+                        labels: {
+                            show:true,
+                        }
+                    }, {
+                        type: "linear",
+                        display: false,
+                        position: "right",
+                        id: "y-axis-2",
+                        ticks: {
+                          stepSize: 10,
+                          beginAtZero: true,
+                          fontColor: '#fff',
+                          suggestedMax: 100,
+                        },
+                        gridLines:{
+                            display: false
+                        },
+                        labels: {
+                            show:false,
+                        }
+                    }]
+                }
+            }
+            });
+
+        };
 });
