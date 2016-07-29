341c5bc3619cfe24fc343ee727b028411421b07e
joanneonis <joanne.onis@hotmail.com>
2016-07-29 09:35:09 +0200

    Revert "test"
    This reverts commit dcab2a5c8a41f694ca9a420d6578b9a2764bd5d1, reversing
    changes made to 10cfe19793bf37f4fb7e5dd8f0fa940d6920848b.

--- public/Assets/src/Js/components/backlog/backlog.js
+++ public/Assets/src/Js/components/backlog/backlog.js
@@ -3,57 +3,9 @@ app.component('backlog', {
         title: '<',
         backTitle: '<'
     },
-    controller(BacklogService, $firebaseAuth) {
+    controller() {
         let ctrl = this;
-        let auth = $firebaseAuth();
-
-        ctrl.state = {
-            New: 0,
-            Approved: 1,
-            Done: 3,
-            Removed: -1
-        };
-
-        ctrl.filter = {};
         ctrl.open = true;
-
-        BacklogService.getBacklog().then(function (data) {
-            ctrl.BiItems = data;
-        });
-
-        ctrl.selectItem = (item) => {
-            ctrl.selectedItem = item;
-        }
-
-        ctrl.addItem = () => {
-            var newItem = {
-                name: "Nieuw...",
-                effort: 0,
-                description: "",
-                order: 0,
-                state: 0
-            }
-
-            BacklogService.add(newItem).then((data) => {
-                ctrl.selectItem(ctrl.BiItems.$getRecord(data.key));
-            });
-        }
-
-        ctrl.deleteItem = (item) => {
-            BacklogService.remove(item);
-        }
-
-        ctrl.saveItem = (item) => {
-            BacklogService.save(item);
-        }
-
-        ctrl.filterItems = function (x) {
-            if (x == ctrl.filter.state) {
-                ctrl.filter.state = null;
-            } else {
-                ctrl.filter.state = x;
-            }
-        }
     },
-    templateUrl: `${templatePath}/backlog.html`
+    templateUrl: `${templatePath}/backlog.html` 
 });  
